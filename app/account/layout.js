c73f0f604dd679939cbf6d8d98542dc6febc9426
BENJAMIN DARTEY <95862472+Benjilo12@users.noreply.github.com>
2024-10-12 11:03:01 +0000

    Adding Nested Layout Next.js

---
+++ app/account/layout.js
@@ -0,0 +1,14 @@
+import SideNavigation from "../_components/SideNavigation";
+
+function layout({ children }) {
+  return (
+    <div className="grid grid-cols-[16rem_1fr] h-full gap-12">
+      <div>
+        <SideNavigation />
+      </div>
+      <div className="py-1">{children}</div>
+    </div>
+  );
+}
+
+export default layout;
